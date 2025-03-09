259ab8da6f2ca034a1f941800b1c809ff7336e0a
akkiirah <46369555+akkiirah@users.noreply.github.com>
2025-03-09 00:28:12 +0000

    Update DeleteTitlebar.bat

--- DeleteTitlebar.bat
+++ DeleteTitlebar.bat
@@ -1,2 +1,2 @@
 @echo off
-PowerShell.exe -WindowStyle Hidden -Command "powershell.exe -ExecutionPolicy Bypass -File 'C:\Users\akkiirah\.glaze-wm\scripts\DeleteTitlebar.ps1' >$null 2>&1"
+PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "%USERPROFILE%\.glzr\scripts\DeleteTitlebar.ps1" >nul 2>&1<
